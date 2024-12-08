2adb48b31fde185e8c1f447e901b8de3334b3770
majharul <majharuljony163@gmail.com>
2024-12-08 15:30:24 +0600

    learn time complexity

---
+++ time-complexity/quadratic_complexity_O_N_N.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mn()
+{
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int m = 0; m < n; m++)
+        {
+            cout << "Hello c++" << endl;
+        }
+    }
+
+    return 0;
+}
+
+// complexity = n*m
+
+int nnn()
+{
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                cout << "Hello c++" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
+// complexity = n*n*n
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Hello c++" << endl;
+        }
+    }
+
+    return 0;
+}
+// complexity = n*n
