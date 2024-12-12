41820e27ba5fe1a2045c64591600472067031b38
majharul <majharuljony163@gmail.com>
2024-12-12 23:13:45 +0600

    start assignment and solved first 4 problem

---
+++ problem-solving/basic/Summation_from_1_to_N.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long int N;
+    cin >> N;
+
+    long long int summation = N * (N + 1) / 2;
+
+    cout << summation << endl;
+
+    return 0;
+}
