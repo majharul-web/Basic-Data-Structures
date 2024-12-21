effee6ea9184efafe7f8b1f9b023a95d58bbe843
majharul <majharuljony163@gmail.com>
2024-12-21 21:35:41 +0600

    learn doubly linke list insert_at_any_position

---
+++ linked-list/doubly-linked-list/doubly-linked-list-operation/insert/insert_at_any_position.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print_forword(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void insert_at_any_position(Node *head, int index, int val)
+{
+    Node *newnode = new Node(val);
+    Node *temp = head;
+    for (int i = 1; i < index; i++)
+    {
+        temp = temp->next;
+    }
+    newnode->next = temp->next;
+    temp->next->prev = newnode;
+    temp->next = newnode;
+    newnode->prev = temp;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_any_position(head, 2, 100);
+    insert_at_any_position(head, 1, 500);
+
+    cout << "printing forword" << endl;
+    print_forword(head);
+
+    return 0;
+}
