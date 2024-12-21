ceb1b8c0abe8ab522907faa792510c668c56a0ae
majharul <majharuljony163@gmail.com>
2024-12-21 21:02:38 +0600

    learn doubly linked list,insert_at_head,insert_at_tail

---
+++ linked-list/doubly-linked-list/introduction-doubly-linked-list/doubly-linked-list.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print_forword(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void print_backword(Node *tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    cout << "printing forword" << endl;
+    print_forword(head);
+
+    cout << "printing backword" << endl;
+    print_backword(tail);
+
+    return 0;
+}
