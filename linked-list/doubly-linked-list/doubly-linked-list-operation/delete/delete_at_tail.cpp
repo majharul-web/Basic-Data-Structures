a14fa62fe7db13077bec81657276e241a3db579c
majharul <majharuljony163@gmail.com>
2024-12-21 22:00:32 +0600

    learn doubly linked list delete_at_head,delete_at_tail

---
+++ linked-list/doubly-linked-list/doubly-linked-list-operation/delete/delete_at_tail.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void print_forword(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void delete_at_tail(Node *&head, Node *&tail)
+{
+    Node *deletenode = tail;
+    tail = tail->prev;
+    delete deletenode;
+    if (tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    delete_at_tail(head, tail);
+    delete_at_tail(head, tail);
+    // delete_at_tail(head, tail);
+
+    cout << "printing forword" << endl;
+    print_forword(head);
+
+    return 0;
+}
