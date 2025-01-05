ebc2adfbab13169d72ebb1021e1a57cde31d2a43
majharul <majharuljony163@gmail.com>
2025-01-05 09:39:26 +0600

    learn stack using doubly linked list

---
+++ stack/stack_implementation_using_doubly_linked_llist.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+    Node(int val)
+    {
+        this->prev = NULL;
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class MyStack
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int val)
+    {
+        sz++;
+        Node *newnode = new Node(val);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+        tail->next = newnode;
+        newnode->prev = tail;
+        tail = newnode;
+    }
+
+    void pop()
+    {
+        if (tail == NULL)
+        {
+            return;
+        }
+        sz--;
+        Node *deletenode = tail;
+        tail = tail->prev;
+        if (tail == NULL)
+        {
+            head = NULL;
+        }
+        else
+        {
+            tail->next = NULL;
+        }
+        delete deletenode;
+    }
+
+    int top()
+    {
+        if (tail == NULL)
+        {
+            throw runtime_error("Stack is empty");
+        }
+        return tail->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        return head == NULL;
+    }
+};
+
+int main()
+{
+    MyStack st;
+
+    int Q;
+    cin >> Q;
+
+    while (Q--)
+    {
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
