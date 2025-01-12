91bf6fdae10320678c30359dfb2f2d68f39ec240
majharul-web <majharuljony163@gmail.com>
2025-01-12 21:47:44 +0600

    learn binary tree input

---
+++ tree/binary-tree/intro-binary-tree/tempCodeRunnerFile.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->val << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n == -1)
+    {
+        cout << "Tree is empty" << endl;
+        return 0;
+    }
+
+    Node *root = new Node(n);
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        int l, r;
+        cin >> l >> r;
+        if (l != -1)
+        {
+            curr->left = new Node(l);
+            q.push(curr->left);
+        }
+        if (r != -1)
+        {
+            curr->right = new Node(r);
+            q.push(curr->right);
+        }
+    }
+    levelorder(root);
+
+    return 0;
+}
