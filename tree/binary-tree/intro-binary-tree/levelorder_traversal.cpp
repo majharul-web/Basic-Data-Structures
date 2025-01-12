ec53ce80cbbc8245a7873b20bfd1c9e04204587a
majharul-web <majharuljony163@gmail.com>
2025-01-12 21:17:16 +0600

    Learn Binary tree Level order traversal

---
+++ tree/binary-tree/intro-binary-tree/levelorder_traversal.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->val << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+}
+
+int main()
+{
+
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    levelorder(root);
+
+    return 0;
+}
