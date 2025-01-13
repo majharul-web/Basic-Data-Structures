b6cb38141f5de226ea4ba87a4085a6b1ed2f1738
majharul <majharuljony163@gmail.com>
2025-01-13 11:12:55 +0600

    learn count node

---
+++ tree/binary-tree/intro-binary-tree/count_node-recursion.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *createbinarytree()
+{
+    int val;
+    cin >> val;
+
+    if (val == -1)
+    {
+        return NULL; // Tree is empty
+    }
+    Node *root = new Node(val);
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        if (l != -1)
+        {
+            curr->left = new Node(l);
+            q.push(curr->left);
+        }
+        if (r != -1)
+        {
+            curr->right = new Node(r);
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+
+void printlevelorder(Node *root)
+{
+    if (!root)
+    {
+        cout << "Tree is empty" << endl;
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->val << " ";
+
+        if (curr->left)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right)
+        {
+            q.push(curr->right);
+        }
+    }
+}
+
+int countnode(Node *root)
+{
+    if (!root)
+    {
+
+        return 0;
+    }
+    int l = countnode(root->left);
+    int r = countnode(root->right);
+    return l + r + 1;
+}
+
+int main()
+{
+    Node *root = createbinarytree();
+
+    cout << "printing tree" << endl;
+    printlevelorder(root);
+    cout << endl;
+    cout << "count : " << countnode(root);
+
+    return 0;
+}
---
+++ tree/binary-tree/intro-binary-tree/count_node.cpp
@@ -0,0 +1,98 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *createbinarytree()
+{
+    int val;
+    cin >> val;
+
+    if (val == -1)
+    {
+        return NULL; // Tree is empty
+    }
+    Node *root = new Node(val);
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        if (l != -1)
+        {
+            curr->left = new Node(l);
+            q.push(curr->left);
+        }
+        if (r != -1)
+        {
+            curr->right = new Node(r);
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+
+int printlevelorder(Node *root)
+{
+    if (!root)
+    {
+        cout << "Tree is empty" << endl;
+        return 0;
+    }
+
+    int count = 0;
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->val << " ";
+        count++;
+
+        if (curr->left)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right)
+        {
+            q.push(curr->right);
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    Node *root = createbinarytree();
+
+    cout << "printing tree" << endl;
+    int count = printlevelorder(root);
+    cout << endl;
+    cout << "count" << endl;
+    cout << count << endl;
+
+    return 0;
+}
