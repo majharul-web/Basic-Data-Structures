16a2594f5ffcf95028752834cb1f14bb38abb18c
majharul <majharuljony163@gmail.com>
2025-01-15 09:06:20 +0600

    learn stl pair

---
+++ stl/stl_pair.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    pair<int, string> p;
+    p = make_pair(10, "roll");
+
+    cout << p.first << endl;
+    cout << p.second << endl;
+
+    return 0;
+}
