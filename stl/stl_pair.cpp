bea071e7576351892a39212fcc23ac491cb532d0
majharul <majharuljony163@gmail.com>
2025-01-15 09:14:31 +0600

    learn pair vector

--- stl/stl_pair.cpp
+++ stl/stl_pair.cpp
@@ -3,12 +3,29 @@ using namespace std;
 
 int main()
 {
+    /*
+        pair<int, string> p;
+        p = make_pair(10, "roll");
 
-    pair<int, string> p;
-    p = make_pair(10, "roll");
+        cout << p.first << endl;
+        cout << p.second << endl;
+    */
 
-    cout << p.first << endl;
-    cout << p.second << endl;
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    cout << "printing pair of vector" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
 
     return 0;
 }
